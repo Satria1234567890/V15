2301d1a0d8ee4d94f07e0094b8de228865cf631e
Rijalbotz <99317707+Rijalbotz@users.noreply.github.com>
2022-03-02 13:31:54 +0700

    Update sewa.js

--- shop/sewa.js
+++ shop/sewa.js
@@ -2,24 +2,32 @@ const pc_sewa = () => {
 return `「 *PRICE LIST* 」
 							
 ⬣ PRICE SEWABOT
-
-⬡ 1 jam test sebelum beli free 
-
-
-⬡ PERMANEN+USER PREM = Rp 7.000
-
-
-
-
-
-
+⬣ PRICE SEWABOT
+*•PRICE SEWA BOT BY RIJAL JB*
+*FITUR NYA GG PASTINYA ON 24 JAM KECUALI DELAY/PAKET HABIS*
+*FITUR :*
+*ANTIVIRTEX ON*
+*WELCOME OTOMATIS*
+*NSFW-HENTAI Dll ON*
+*ADA FITUR DOWNLOAD TT.IG.FB.YT*
+*Dll Fitur-fitur Menariknya*
+*HARGA SEWA BOT :*
+*5K 7 Day*
+*~7K 7 Day+Prem*
+*10k 30 Day*
+*~12k 30 Day+Prem*
+*[SPESIAL]15k Manen+Prem*
+*wa.me/+6283159252524?text=Assalamu'alaikum+Bang+Mau+Sewa+BOT*
+*Gunakan BOT DENGAN BIJAK 👍🏼*
+*LINK GC :https://chat.whatsapp.com/GNRIi7Cfp8hGNsTlAEiuUD*
 ⬣ MINAT? PM
-⬡ wa.me/6285803583481
-
+⬡ wa.me/6283159252524
 
 NOTE : 
-Bot On 24 Jam Karna Di Run Menggunakan RDP
+Bot On 24 Jam Karna Di Run Menggunakan Heroku
 Reset Data Setiap Jam 02:00 `
 	}
+
+
 
 exports.pc_sewa = pc_sewa
